81512e4df88be202852e4c42f50332b19f55c84b
Alysha Cezette-Thompson <alyshatcezette@gmail.com>
2024-01-10 18:22:21 -0500

    Fixed wrong files

--- script.js
+++ script.js
@@ -1,106 +1,77 @@
-function nameValidator(){
-    const nameInput = document.getElementById("name");
-    const letters = /^[A-Za-z\s]+$/;
+$(document).ready(function() {
 
-    if(!letters.test(nameInput.value)){
-        alert("Name must only include letters");
-        return false;
-    }
-    return true;
-}
+    $("#unexpand").hide(); 
 
-function phoneValidator(){
-    const phoneInput = document.getElementById("phone");
-    const phoneForm = /^\(\d{3}\) \d{3}-\d{4}$/;
+    $("#body").click(function() {
+        $("#image1").toggle(); 
+    });
 
-    if(!phoneForm.test(phoneInput.value)){
-        alert("Phone number must be in form (416) 555-5555");
-        return false;
-    }
-    return true;
+    $("#eyes").click(function() {
+        $("#image2").toggle();
+    });
 
-}
+    $("#hat").click(function() {
+        $("#image3").toggle(); 
+    });
 
-function phoneConverter(phone){
-    const regex = /\((\d{3})\)\s(\d{3})-(\d{4})/;
-  
-    const formattedPhoneNumber = phone.replace(regex, '$1-$2-$3');
-    
-    return formattedPhoneNumber;
-}
+    $("#mouth").click(function() {
+        $("#image4").toggle(); 
+    });
 
+    $("#nose").click(function() {
+        $("#image5").toggle(); 
+    });
 
-function formValidator(){
-   
-    if(nameValidator() && phoneValidator()){
-        const nameDisplay = document.getElementById("name").value;
-        const addressDisplay = document.getElementById("address").value;
-        const phoneDisplay = document.getElementById("phone").value;
-        const display = document.getElementById("display");
+    $("#save").click(function() {
+        html2canvas(document.querySelector("#imageContainer"), { useCORS: true }).then((canvas) =>{
+            var imgData = canvas.toDataURL("image/png");
+            console.log(imgData)
+            var a = document.createElement("a");
+            a.href = imgData;
+            a.download = "image.png";
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        });
+    });
 
-        let displayHTML= `<p>` + nameDisplay + `</p>` + `<p>` + addressDisplay + `</p>` + `<p>` + phoneConverter(phoneDisplay) + `</p>`; 
-   
-        display.innerHTML = displayHTML;
-        display.setAttribute("style", "width: 200px; border: solid #e288a6 4px; text-align: center;") 
-    }
-}
+    let isExpanded = false;
 
-function countLetter(){
-    const letter = document.getElementById("letterCount");
-    const textArea = document.getElementById("text").value;
-    let count = 0 ;
-
-    for (const char of textArea) {
-        const nums = /[A-Za-z]/;
-        if(nums.test(char)){
-            count++;
-        }
-    }
-
-    letter.innerHTML= count;
-    
-}
-
-
-function countChars(){
-    const chars = document.getElementById("charCount");
-    const textArea = document.getElementById("text").value;
-
-    chars.innerHTML= textArea.length;
-}
-
-
-function count(){
-    countChars(); 
-    countLetter();
-}
-
-let urls = ["https://www.youtube.com/", "http://www.pnwx.com/", "https://www.jellycat.com/us/"];
-
-function createBookmark(){
-    const bookmarksList = document.getElementById("bookmarks");
-
-    for (let url of urls) {
-        let icon, style;
+    $("#cat").on("mouseenter", (function() {
+        if (!isExpanded) {
+            $("#cat").animate({
+                width: "100%",
+                height: "auto"
+            },
+            {
+                duration: 2000,
+                complete: function () {
+                    $("#unexpand").show();
+                    isExpanded = false;
+                }
+            }
+            );
         
-        if (url.startsWith("https://")) {
-            icon = "closed.png";
-            style = "width: auto; height: 1em;"
-        } else if (url.startsWith("http://")) {
-            icon = "open.png";
-            style = "width: auto; height: 1em;"
-        } else {
-            alert("Not a valid URL: " + url);
-            return;
+            isExpanded = true;
         }
+    }));
 
-        const listItem = document.createElement("li");
-        listItem.innerHTML = `<li><img src="assets/${icon}" style="${style}"> <a href=${url}>${url}</li>`;
-        bookmarksList.appendChild(listItem);
-    }
-}
-createBookmark();
+    
+    $("#unexpand").click(function() {
+        if (!isExpanded) {
+            $("#unexpand").hide();
+            $("#cat").animate({
+                width: "100px",
+                height: "auto"
+            },
+            {
+                duration: 2000,
+                complete: function () {
+                    isExpanded = false;
+                }
+            });
+            isExpanded = true;
+        }
+    });
 
-
-
-
+});
